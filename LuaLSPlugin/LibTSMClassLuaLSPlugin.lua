5065e5f403e3c71cc95cf4029cf7593fca5f00ff
Sapu TradeSkillMaster <sapu94@gmail.com>
2023-10-30 07:54:35 -0700

    LuaLS plugin optimizations and support for __static function assignment

--- LuaLSPlugin/LibTSMClassLuaLSPlugin.lua
+++ LuaLSPlugin/LibTSMClassLuaLSPlugin.lua
@@ -1,36 +1,56 @@
 function OnSetText(uri, text)
     local diffs = {}
 
-    -- Add static class methods to the class
-    for startPos, endPos in text:gmatch("function [A-Za-z0-9_]+()%.__static().[A-Za-z0-9_]+%(") do
+    -- Look for function definitions
+    for lineStart, modifierStart, modifier, modifierEnd, colonOrDot in text:gmatch("()function [A-Za-z0-9_]+()%.__([a-z]+)()([:%.])[A-Za-z0-9_]+%(") do
+        if modifier == "static" and colonOrDot == "." then
+            -- Add static class methods to the class
+            diffs[#diffs+1] = {
+                start = modifierStart,
+                finish = modifierEnd - 1,
+                text = "",
+            }
+        elseif modifier == "private" or modifier == "protected" then
+            diffs[#diffs+1] = {
+                start = lineStart,
+                finish = lineStart - 1,
+                text = "---@"..modifier.."\n",
+            }
+            diffs[#diffs+1] = {
+                start = modifierStart,
+                finish = modifierEnd - 1,
+                text = "",
+            }
+        elseif modifier == "__protected" then
+            diffs[#diffs+1] = {
+                start = lineStart,
+                finish = lineStart - 1,
+                text = "---@protected\n",
+            }
+            diffs[#diffs+1] = {
+                start = modifierStart,
+                finish = modifierEnd - 1,
+                text = "",
+            }
+        end
+    end
+
+    -- Look for static function assignment
+    for startPos, endPos in text:gmatch("[A-Za-z0-9_]+()%.__static()%.[A-Za-z0-9_]+ = ") do
         diffs[#diffs+1] = {
             start = startPos,
             finish = endPos - 1,
-            text = '',
+            text = "",
         }
     end
 
-    -- Add private class methods to the class and mark them as private
-    for lineStart, modifierStart, modifierEnd in text:gmatch("()function [A-Za-z0-9_]+()%.__private():[A-Za-z0-9_]+%(") do
-        diffs[#diffs+1] = {
-            start = lineStart,
-            finish = lineStart - 1,
-            text = '---@private\n',
-        }
-        diffs[#diffs+1] = {
-            start = modifierStart,
-            finish = modifierEnd - 1,
-            text = '',
-        }
-    end
-
-    -- Mark private class members which are set in the constructor and denoted by a leading '_' as private
-    for funcStart, funcContent in text:gmatch("function [A-Za-z0-9_]+%.?[_a-z]*:__init%([^%)]*%)()(.-)\nend") do
+    -- Mark class members which are set in the constructor and denoted by a leading "_" as private
+    for funcStart, funcContent in text:gmatch("function [A-Za-z0-9_]+%.?[_a-z]*[:%.]__init%([^%)]*%)()(.-)\nend") do
         for lineStart in funcContent:gmatch("()self%._[a-zA-Z0-9_]+%s*=") do
             diffs[#diffs+1] = {
                 start = funcStart + lineStart - 1,
                 finish = funcStart + lineStart - 2,
-                text = '---@private\n',
+                text = "---@private\n",
             }
         end
     end
