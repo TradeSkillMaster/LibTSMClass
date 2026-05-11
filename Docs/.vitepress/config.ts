39932d27041842d7928671cb6cfda95d9ef6d59a
Sapu94 <sapu@tradeskillmaster.com>
2026-05-11 07:26:15 -0700

    Add vitepress docs

---
+++ Docs/.vitepress/config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitepress"
+
+export default defineConfig({
+	title: "LibTSMClass",
+	description: "OOP class library for World of Warcraft Lua addons",
+	themeConfig: {
+		nav: [
+			{ text: "Home", link: "/" },
+		],
+		sidebar: [
+			{
+				items: [
+					{ text: "Home", link: "/" },
+					{ text: "Features", link: "/features" },
+					{ text: "Notes", link: "/notes" },
+					{ text: "API", link: "/api" },
+				],
+			},
+		],
+	},
+})
